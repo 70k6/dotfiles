68124aa9fc59dc85b16ff91f19200331e9cefb7d
70k6 <70k666@gmail.com>
2017-05-16 21:26:25 +0800

    add brew

---
+++ brew.sh
@@ -0,0 +1,19 @@
+brew install vim 
+brew install zsh
+brew install wget
+brew install ack
+brew install git
+brew install tig
+brew install tree
+brew install youtube-dl
+brew install python3
+brew install go
+brew install node
+
+brew cask install iterm2
+brew cask install google-chrome
+brew cask install spectacle
+brew cask install dropbox
+brew cask install astrill
+brew cask install iina
+
